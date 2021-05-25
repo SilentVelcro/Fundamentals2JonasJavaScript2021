f2e20b5424c69acf79b9d1f78690febc428a2f75
Nikki Grostefon <73565265+NikkiGrostefon@users.noreply.github.com>
2021-05-25 13:04:23 -0400

    starting fuctions calling other functions

--- script.js
+++ script.js
@@ -39,7 +39,6 @@ console.log(appleOrangeJuice); // another way
 
 const num = Number('23'); // built in functions exist 
 console.log(num);
-*/
 
 ///////////////////////////////////////////////////////// FUNCTION DECLARATIONS VS EXPRESSIONS 34
 
@@ -63,7 +62,23 @@ const calcAge2 = function(birthYear) { // a function without a name is a annoyno
 const age2 = calcAge2(1980);
 
 console.log(age1, age2);
-
+*/
 ///////////////////////////////////////////////////////// ARROW FUNCTION 35
+// Arrow functions do not get a so-called this keyword.
 
+const calcAge3 = birthYear => 2037 - birthYear; //birthYear is parameter
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    // return retirement
+    return `${firstName} retires in ${retirement} years.`;
+}
+
+console.log (yearsUntilRetirement(1991, 'Nikki'));
+console.log (yearsUntilRetirement(1980, 'Susan'));
+
+///////////////////////////////////////////////////////// FUNCTIONS CALLING OTHER FUNCTIONS 36
 
