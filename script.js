dda867949f050e6cc5b4077529fdfa2f32e6011b
Nikki Grostefon <73565265+NikkiGrostefon@users.noreply.github.com>
2021-05-25 19:44:54 -0400

    chapter 37 completed

--- script.js
+++ script.js
@@ -94,5 +94,26 @@ return juice;
 }
 
 console.log(fruitProcessor(2, 3));
-*/
+
 ///////////////////////////////////////////////////////// REVIEWING FUCTIONS 37
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+ const age = calcAge(birthYear);
+ const retirement = 65 - age;
+        
+if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years.`);
+        return retirement;
+} else {
+    console.log(`${firstName} has already retired.`);
+        return -1;
+}
+}
+console.log(yearsUntilRetirement(1980, 'Nikki'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
+*/
+/////////////////////////////////////////////////////////
